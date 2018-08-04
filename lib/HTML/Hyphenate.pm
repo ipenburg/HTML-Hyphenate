b436bd41ac2a920632fb17bf4e986cf285830ab9
Roland van Ipenburg <roland.vanipenburg@puurpxl.nl>
2018-08-04 15:24:59 +0200

    chore: bump version

--- lib/HTML/Hyphenate.pm
+++ lib/HTML/Hyphenate.pm
@@ -1,6 +1,6 @@
 # -*- cperl; cperl-indent-level: 4 -*-
 # Copyright (C) 2009-2018, Roland van Ipenburg
-package HTML::Hyphenate v1.0.0;
+package HTML::Hyphenate v1.1.0;
 
 use strict;
 use warnings;
@@ -274,7 +274,7 @@ HTML::Hyphenate - insert soft hyphens into HTML.
 
 =head1 VERSION
 
-This document describes HTML::Hyphenate version v1.0.0.
+This document describes HTML::Hyphenate version v1.1.0.
 
 =head1 SYNOPSIS
 
